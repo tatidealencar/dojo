ef761621736a7753d48b9f3d68d411844fc4e707
FabioCT <fabioturchetto@hotmail.com>
2021-07-22 18:47:57 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 var noticiasJSON = '{"1": {"titulo": "","subtitulo": "","data-hora": "","autor": "","foto-capa": "","texto": ""},"2": {"titulo": "","subtitulo": "","data-hora": "","autor": "","foto-capa": "","texto": ""},"3": {"titulo": "","subtitulo": "","data-hora": "","autor": "","foto-capa": "","texto": ""},"4": {"titulo": "","subtitulo": "","data-hora": "","autor": "","foto-capa": "","texto": ""},"5": {"titulo": "","subtitulo": "","data-hora": "","autor": "","foto-capa": "","texto": ""}}';
 
 console.log(JSON.parse(noticiasJSON));
+
+console.log("teste")
