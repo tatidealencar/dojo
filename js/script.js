e0a674d8e37bc66949080960d4066561361e6927
tatidealencar <tatidealencar@gmail.com>
2021-07-22 17:59:50 -0300

    Alteração do script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
 var noticiasJSON = '{"1": {"titulo": "","subtitulo": "","data-hora": "","autor": "","foto-capa": "","texto": ""},"2": {"titulo": "","subtitulo": "","data-hora": "","autor": "","foto-capa": "","texto": ""},"3": {"titulo": "","subtitulo": "","data-hora": "","autor": "","foto-capa": "","texto": ""},"4": {"titulo": "","subtitulo": "","data-hora": "","autor": "","foto-capa": "","texto": ""},"5": {"titulo": "","subtitulo": "","data-hora": "","autor": "","foto-capa": "","texto": ""}}';
 
 console.log(JSON.parse(noticiasJSON));
-
-var noticia;
