3a69364f3f8b43a250e789e8873c09fcd91fe9c2
Tatiana Alencar <tatidealencar@gmail.com>
2021-07-29 19:30:22 -0300

    Diversos

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
     fetch("../data.json")
         .then(function (response) {
             response.json().then(function(json) {
+
+                //Economia
                 var categoria_economia = document.querySelector('#categoria_economia');
                 var array = Object.keys(json).map(i => json[Number(i)]);
                 for (let i = 0; i < Object.keys(array).length; i++) {
@@ -10,6 +12,18 @@ document.addEventListener('DOMContentLoaded', function () {
                         li.classList.add('list-group-item');
                         li.textContent = array[i]['titulo'];
                         categoria_economia.appendChild(li);
+                    }
+                }
+
+                //Política
+                var categoria_politica = document.querySelector('#categoria_politica');
+                var array = Object.keys(json).map(i => json[Number(i)]);
+                for (let i = 0; i < Object.keys(array).length; i++) {
+                    if (array[i]['categoria'] == 2) {
+                        var li = document.createElement('li');
+                        li.classList.add('list-group-item');
+                        li.textContent = array[i]['titulo'];
+                        categoria_politica.appendChild(li);
                     }
                 }
             });
