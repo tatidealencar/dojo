cb5403982f13f84f56e3d434baa84358f9817e69
tatidealencar <tatidealencar@gmail.com>
2021-07-29 18:17:15 -0300

    Noticias JSON

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-window.onload(function() {
-    var mydata = JSON.parse(data);
-
-    console.log(mydata);
+document.addEventListener('DOMContentLoaded', function () {
+    fetch("../data.json")
+        .then(function (response) {
+            response.json().then(function(json) {
+                var categoria_economia = document.querySelector('#categoria_economia');
+                var array = Object.keys(json).map(i => json[Number(i)]);
+                for (let i = 0; i < Object.keys(array).length; i++) {
+                    if (array[i]['categoria'] == 1) {
+                        var li = document.createElement('li');
+                        li.classList.add('list-group-item');
+                        li.textContent = array[i]['titulo'];
+                        categoria_economia.appendChild(li);
+                    }
+                }
+            });
+        })
 });
