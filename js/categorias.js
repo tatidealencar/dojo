cd41c87e8444c988fb09c129bf5338c7e89880f1
Tatiana Alencar <tatidealencar@gmail.com>
2021-08-03 21:35:54 -0300

    Melhorias nos scripts

--- js/categorias.js
+++ js/categorias.js
@@ -1,17 +1,207 @@
-document.addEventListener('DOMContentLoaded', function () {
+var parametro = new URLSearchParams(document.location.search.substring(1));
+var categoria = parametro.get('categoria');
+
+setCategoria(categoria);
+carregaNoticias(categoria);
+
+function criarCard(noticias, subcategoria, destaque, row, cor) {
+
+    if (noticias.length > 0) {
+        var foto_capa = "";
+        var descricao_foto_capa = "";
+        var titulo = "";
+        var subtitulo = "";
+        var data_hora = "";
+
+        if (destaque.length > 0) {
+            foto_capa = destaque[0]['foto-capa'];
+            descricao_foto_capa = destaque[0]['descricao-foto-capa'];
+            titulo = destaque[0]['titulo'];
+            subtitulo = destaque[0]['subtitulo'];
+            data_hora = destaque[0]['data-hora'];
+        } else {
+            foto_capa = noticias[0]['foto-capa'];
+            descricao_foto_capa = noticias[0]['descricao-foto-capa'];
+            titulo = noticias[0]['titulo'];
+            subtitulo = noticias[0]['subtitulo'];
+            data_hora = noticias[0]['data-hora'];
+        }
+
+        var col = document.createElement('col');
+        col.classList.add('col');
+
+        var card = document.createElement('card');
+        card.classList.add('card');
+
+        var img = document.createElement('img');
+        img.classList.add('card-img-top');
+        img.setAttribute('src', foto_capa);
+        img.setAttribute('alt', descricao_foto_capa);
+
+        var card_image = document.createElement('div');
+        card_image.classList.add('card-img-overlay', 'd-flex', 'flex-row-reverse');
+
+        var h5 = document.createElement('h5');
+        h5.classList.add('card-title', 'p-2', 'position-absolute', 'end-0', 'badge-img-categoria');
+        h5.textContent = subcategoria;
+        h5.classList.add(cor);
+
+        card_image.appendChild(h5);
+
+        var card_body = document.createElement('div');
+        card_body.classList.add('card-body', 'bg-card');
+
+        var card_title = document.createElement('h5');
+        card_title.classList.add('card-title');
+        card_title.textContent = titulo;
+
+        var card_text = document.createElement('p');
+        card_text.classList.add('card-text');
+        card_text.textContent = subtitulo;
+
+        var card_small = document.createElement('small');
+        card_small.classList.add('text-muted');
+        card_small.textContent = "Publicado dia " + data_hora;
+
+        card_body.appendChild(card_title);
+        card_body.appendChild(card_text);
+        card_body.appendChild(card_small);
+        card.appendChild(img);
+        card.appendChild(card_image);
+        card.appendChild(card_body);
+
+        if (noticias.length > 1) {
+            var list_group = document.createElement('div');
+            list_group.classList.add('list-group', 'list-group-flush');
+            noticias.forEach(function(element, index) {
+                if (element['destaque'] != 'true') {
+                    let link = document.createElement('a');
+                    link.classList.add('list-group-item', 'list-group-item-action');
+                    link.textContent = element['titulo'];
+                    link.setAttribute('href', 'noticia.html?id=' + index);
+                    list_group.appendChild(link);
+                }
+
+            });
+            card.appendChild(list_group);
+        }
+
+        col.appendChild(card);
+        row.appendChild(col);
+    }
+}
+
+Array.prototype.filterAssoc = function (callback) {
+    var a = this.slice();
+    a.map(function (v, k, a) {
+        if (!callback(v, k, a)) delete a[k];
+    });
+    return a;
+};
+
+function carregaNoticias(categoria) {
+
     fetch("../data.json")
         .then(function (response) {
             response.json().then(function (json) {
                 var array = Object.keys(json).map(i => json[Number(i)]);
+                var subcategorias = [];
+                var noticias = [];
+                for (let i = 0; i < Object.keys(array).length; i++) {
+                    if (array[i]['categoria'] == categoria) {
+                        noticias.push(array[i]);
+                        if (subcategorias.includes(array[i]['subcategoria']) == false) {
+                            subcategorias.push(array[i]['subcategoria']);
+                        }
+                    }
+                }
 
-                //percorrer o array e pegar somente as notícias da categoria desejada
+                var bloco = document.getElementById('bloco');
+                for (let i = 0; i < subcategorias.length; i = i + 3) {
+                    var row = document.createElement('div');
+                    row.classList.add('row', 'row-cols-1', 'row-cols-sm-2', 'row-cols-md-3');
 
-                //agrupar as notícias em subcategorias
+                    let j = i;
 
-                //para cada subcategoria, criar um card
+                    for (j; j <= i + 2 && j < subcategorias.length; j++) {
+                        criarCard(noticias.filterAssoc(function (noticia) {
+                            return noticia['subcategoria'] == subcategorias[j]
+                        }), subcategorias[j], noticias.filter(function (noticia) {
+                            return noticia['destaque'] == 'true' && noticia['subcategoria'] == subcategorias[j]
+                        }), row, getCor(categoria));
+                    }
 
-                //para cada conjunto de notícias de uma determinada subcategoria, criar lista de notícias
+                    bloco.appendChild(row);
+
+                    if (i < subcategorias.length - 3) {
+                        let hr = document.createElement('hr');
+                        hr.classList.add('card-line');
+                        bloco.append(hr);
+                    }
+
+
+                }
 
             });
         })
-});
+}
+
+function getCor(categoria) {
+    let cor = ({
+        1: "bg-economia",
+        2: "bg-politica",
+        3: "bg-entretenimento",
+        4: "bg-esporte",
+        5: "bg-tecnologia",
+        6: "bg-saude",
+        7: "bg-viagem-gastro",
+        8: "bg-educacao",
+        9: "bg-carros"
+
+    })[categoria];
+
+    return cor;
+}
+
+function setCategoria(categoria) {
+
+    var nome_categoria = document.getElementById('nome_categoria');
+
+    switch (categoria) {
+        case '1':
+            nome_categoria.innerText = "Economia";
+            let link_economia = document.getElementById('link_economia');
+            link_economia.classList.add('active');
+            break;
+        case '2':
+            nome_categoria.innerText = "Política";
+            let link_politica = document.getElementById('link_politica');
+            link_politica.classList.add('active');
+            break;
+        case '3':
+            nome_categoria.innerText = "Entretenimento";
+            let link_entretenimento = document.getElementById('link_entretenimento');
+            link_entretenimento.classList.add('active');
+            break;
+        case '4':
+            nome_categoria.innerText = "Esporte";
+            let link_esporte = document.getElementById('link_esporte');
+            link_esporte.classList.add('active');
+            break;
+        case '5':
+            nome_categoria.innerText = "Tecnologia";
+            break;
+        case '6':
+            nome_categoria.innerText = "Saúde";
+            break;
+        case '7':
+            nome_categoria.innerText = "Viagens & Gastronomia";
+            break;
+        case '8':
+            nome_categoria.innerText = "Educação";
+            break;
+        case '9':
+            nome_categoria.innerText = "Carros";
+            break;
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,107 +1,100 @@
 document.addEventListener('DOMContentLoaded', function () {
     fetch("../data.json")
         .then(function (response) {
-            response.json().then(function(json) {
+            response.json().then(function (json) {
 
-                //Economia
                 var categoria_economia = document.querySelector('#categoria_economia');
-                var array = Object.keys(json).map(i => json[Number(i)]);
-                for (let i = 0; i < Object.keys(array).length; i++) {
-                    if (array[i]['categoria'] == 1) {
-                        
-                        var li = document.createElement('li');
-                        li.classList.add('list-group-item');
-                        li.textContent = array[i]['titulo'];
-                        categoria_economia.appendChild(li);
-                    }
-                }
-
-                //Política
                 var categoria_politica = document.querySelector('#categoria_politica');
-                var array = Object.keys(json).map(i => json[Number(i)]);
-                for (let i = 0; i < Object.keys(array).length; i++) {
-                    if (array[i]['categoria'] == 2) {
-                        var li = document.createElement('li');
-                        li.classList.add('list-group-item');
-                        li.textContent = array[i]['titulo'];
-                        categoria_politica.appendChild(li);
-                    }
-                }
-                //Entretenimento
                 var categoria_entretenimento = document.querySelector('#categoria_entretenimento');
-                var array = Object.keys(json).map(i => json[Number(i)]);
-                for (let i = 0; i < Object.keys(array).length; i++) {
-                    if (array[i]['categoria'] == 3) {
-                        var li = document.createElement('li');
-                        li.classList.add('list-group-item');
-                        li.textContent = array[i]['titulo'];
-                        categoria_entretenimento.appendChild(li);
-                    }
-                }
-                //Esporte
                 var categoria_esporte = document.querySelector('#categoria_esporte');
-                var array = Object.keys(json).map(i => json[Number(i)]);
-                for (let i = 0; i < Object.keys(array).length; i++) {
-                    if (array[i]['categoria'] == 4) {
-                        var li = document.createElement('li');
-                        li.classList.add('list-group-item');
-                        li.textContent = array[i]['titulo'];
-                        categoria_esporte.appendChild(li);
-                    }
-                }
-                //Tecnologia
                 var categoria_tecnologia = document.querySelector('#categoria_tecnologia');
-                var array = Object.keys(json).map(i => json[Number(i)]);
-                for (let i = 0; i < Object.keys(array).length; i++) {
-                    if (array[i]['categoria'] == 5) {
-                        var li = document.createElement('li');
-                        li.classList.add('list-group-item');
-                        li.textContent = array[i]['titulo'];
-                        categoria_tecnologia.appendChild(li);
-                    }
-                }
-                //Saúde
                 var categoria_saude = document.querySelector('#categoria_saude');
-                var array = Object.keys(json).map(i => json[Number(i)]);
-                for (let i = 0; i < Object.keys(array).length; i++) {
-                    if (array[i]['categoria'] == 6) {
-                        var li = document.createElement('li');
-                        li.classList.add('list-group-item');
-                        li.textContent = array[i]['titulo'];
-                        categoria_saude.appendChild(li);
-                    }
-                }
-                //Viagem e Turismo
                 var categoria_viagem = document.querySelector('#categoria_viagem');
-                var array = Object.keys(json).map(i => json[Number(i)]);
-                for (let i = 0; i < Object.keys(array).length; i++) {
-                    if (array[i]['categoria'] == 7) {
-                        var li = document.createElement('li');
-                        li.classList.add('list-group-item');
-                        li.textContent = array[i]['titulo'];
-                        categoria_viagem.appendChild(li);
-                    }
-                }
-                //Educação
                 var categoria_educacao = document.querySelector('#categoria_educacao');
-                var array = Object.keys(json).map(i => json[Number(i)]);
-                for (let i = 0; i < Object.keys(array).length; i++) {
-                    if (array[i]['categoria'] == 8) {
-                        var li = document.createElement('li');
-                        li.classList.add('list-group-item');
-                        li.textContent = array[i]['titulo'];
-                        categoria_educacao.appendChild(li);
-                    }
-                }
-                //Carros
                 var categoria_carros = document.querySelector('#categoria_carros');
+
                 var array = Object.keys(json).map(i => json[Number(i)]);
                 for (let i = 0; i < Object.keys(array).length; i++) {
+
+                    if (array[i]['categoria'] == 1) {
+                        if (categoria_economia.getElementsByTagName('li').length < 4) {
+                            var li = document.createElement('li');
+                            li.classList.add('list-group-item');
+                            li.textContent = array[i]['titulo'];
+                            categoria_economia.appendChild(li);
+                        }
+                    }
+
+                    if (array[i]['categoria'] == 2) {
+                        if (categoria_politica.getElementsByTagName('li').length < 4) {
+                            var li = document.createElement('li');
+                            li.classList.add('list-group-item');
+                            li.textContent = array[i]['titulo'];
+                            categoria_politica.appendChild(li);
+                        }
+                    }
+
+                    if (array[i]['categoria'] == 3) {
+                        if (categoria_entretenimento.getElementsByTagName('li').length < 4) {
+                            var li = document.createElement('li');
+                            li.classList.add('list-group-item');
+                            li.textContent = array[i]['titulo'];
+                            categoria_entretenimento.appendChild(li);
+                        }
+                    }
+
+                    if (array[i]['categoria'] == 4) {
+                        if (categoria_esporte.getElementsByTagName('li').length < 4) {
+                            var li = document.createElement('li');
+                            li.classList.add('list-group-item');
+                            li.textContent = array[i]['titulo'];
+                            categoria_esporte.appendChild(li);
+                        }
+                    }
+
+                    if (array[i]['categoria'] == 5) {
+                        if (categoria_tecnologia.getElementsByTagName('li').length < 4) {
+                            var li = document.createElement('li');
+                            li.classList.add('list-group-item');
+                            li.textContent = array[i]['titulo'];
+                            categoria_tecnologia.appendChild(li);
+                        }
+                    }
+
+                    if (array[i]['categoria'] == 6) {
+                        if (categoria_saude.getElementsByTagName('li').length < 4) {
+                            var li = document.createElement('li');
+                            li.classList.add('list-group-item');
+                            li.textContent = array[i]['titulo'];
+                            categoria_saude.appendChild(li);
+                        }
+                    }
+
+                    if (array[i]['categoria'] == 7) {
+                        if (categoria_viagem.getElementsByTagName('li').length < 4) {
+                            var li = document.createElement('li');
+                            li.classList.add('list-group-item');
+                            li.textContent = array[i]['titulo'];
+                            categoria_viagem.appendChild(li);
+                        }
+                    }
+
+                    if (array[i]['categoria'] == 8) {
+                        if (categoria_educacao.getElementsByTagName('li').length < 4) {
+                            var li = document.createElement('li');
+                            li.classList.add('list-group-item');
+                            li.textContent = array[i]['titulo'];
+                            categoria_educacao.appendChild(li);
+                        }
+                    }
+
                     if (array[i]['categoria'] == 9) {
-                        var li = document.createElement('li');
-                        li.classList.add('list-group-item');
-                        li.textContent = array[i]['titulo'];
-                        categoria_carros.appendChild(li);
+                        if (categoria_carros.getElementsByTagName('li').length < 4) {
+                            var li = document.createElement('li');
+                            li.classList.add('list-group-item');
+                            li.textContent = array[i]['titulo'];
+                            categoria_carros.appendChild(li);
+                        }
                     }
                 }
             });
